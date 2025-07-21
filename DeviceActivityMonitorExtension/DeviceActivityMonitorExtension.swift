c0088a49651e6b422128d9171bea4a0097bddf4e
DC <>
2025-07-21 17:09:41 +0200

    remove comments

--- DeviceActivityMonitorExtension/DeviceActivityMonitorExtension.swift
+++ DeviceActivityMonitorExtension/DeviceActivityMonitorExtension.swift
@@ -14,47 +14,37 @@ import FamilyControls
 // Optionally override any of the functions below.
 // Make sure that your class name matches the NSExtensionPrincipalClass in your Info.plist.
 class DeviceActivityMonitorExtension: DeviceActivityMonitor {
+  
+  //MARK: - Interval Start/End
   override func intervalDidStart(for activity: DeviceActivityName) {
     super.intervalDidStart(for: activity)
-    
-    // Handle the start of the interval.
-    print("intervalDidStart \n\(activity)")
-    //    DarwinNotificationManager.shared.postNotification(name: "com.yourapp.BroadcastStarted")
-    
-    scheduleNotification(with: "The monitor is now running",
+
+    scheduleNotification(with: "The monitoring session has started",
                          details: "\(activity.rawValue)")
   }
   
   override func intervalDidEnd(for activity: DeviceActivityName) {
     super.intervalDidEnd(for: activity)
     
-    // Handle the end of the interval.
-    print("intervalDidEnd \n\(activity)")
-//    if activity.rawValue == DeviceActivityName.daily.rawValue {
-//      DeviceActivityService.shared.stopAppRestrictions()
-//    }
-    
     DeviceActivityService.shared.stopAppRestrictions()
     
-    //    DarwinNotificationManager.shared.postNotification(name: "com.yourapp.BroadcastStopped")
     scheduleNotification(with: "The monitoring session has finished",
                          details: "\(activity.rawValue)")
   }
   
+  //MARK: - Threshold
   override func eventDidReachThreshold(_ event: DeviceActivityEvent.Name, activity: DeviceActivityName) {
     super.eventDidReachThreshold(event, activity: activity)
     
-    // Handle the event reaching its threshold.
-    print("eventDidReachThreshold \n\(activity)")
-    
-    if event.rawValue == DeviceActivityEvent.Name.block.rawValue {
-      DeviceActivityService.shared.stopAppRestrictions()
-      scheduleNotification(with: "Phone Jail", details: "Congrats! You've reached the end of Restriction Mode (eventDidReachThreshold)")
-    }
+//    if event.rawValue == DeviceActivityEvent.Name.block.rawValue {
+//      DeviceActivityService.shared.stopAppRestrictions()
+//      scheduleNotification(with: "Phone Jail",
+//                           details: "Congrats! You've reached the end of Restriction Mode (eventDidReachThreshold)")
+//    }
 
-    // Получаем сохранённые данные о выбранных приложениях
     if let selection = SharedData.selectedFamilyActivity {
-      //      DarwinNotificationManager.shared.postNotification(name: "com.antisocial.Broadcast.eventDidReachThreshold")
+      
+      //Schedule Interruption for 2 minutes
       if event.rawValue == DeviceActivityEvent.Name.interruption.rawValue {
         BlockingNotificationServiceWithoutSaving.shared.startBlocking(
           hours: 0 ,
@@ -66,50 +56,32 @@ class DeviceActivityMonitorExtension: DeviceActivityMonitor {
       
       for application in selection.applications {
         if let displayName = application.localizedDisplayName {
-          print("Приложение достигло лимита: \(displayName)")
           scheduleNotification(with: "Hey! Time to take a break from this app", details: displayName)
         } else {
-          print("Не удалось получить отображаемое имя приложения")
           scheduleNotification(with: "Phone Jail", details: "Hey! Time to take a break from this app")
         }
       }
-    } else {
-      print("Не удалось получить данные о выбранных приложениях")
     }
   }
   
+  //MARK: - Interval Warnings
   override func intervalWillStartWarning(for activity: DeviceActivityName) {
     super.intervalWillStartWarning(for: activity)
-    
     // Handle the warning before the interval starts.
-    print("intervalWillStartWarning \n\(activity)")
-    scheduleNotification(with: "intervalWillStartWarning", details: "\(activity)")
-    
   }
   
   override func intervalWillEndWarning(for activity: DeviceActivityName) {
     super.intervalWillEndWarning(for: activity)
-    
     // Handle the warning before the interval ends.
-    print("intervalWillEndWarning \n\(activity)")
-    
-    scheduleNotification(with: "intervalWillEndWarning", details: "\(activity)")
   }
   
   override func eventWillReachThresholdWarning(_ event: DeviceActivityEvent.Name, activity: DeviceActivityName) {
     super.eventWillReachThresholdWarning(event, activity: activity)
     
     // Handle the warning before the event reaches its threshold.
-    print("eventWillReachThresholdWarning \n\(event) \n\(activity)")
-    
-    DarwinNotificationManager.shared.postNotification(name: "com.yourapp.ReachThresholdWarning")
-    
-    scheduleNotification(with: "eventWillReachThresholdWarning", details: "\(event) \(activity)")
   }
   
   //MARK: - Notifications
-  //  let userDefaultsKey = "FamilyActivitySelection"
-  
   func scheduleNotification(with title: String, details: String = "") {
     let center = UNUserNotificationCenter.current()
     center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
